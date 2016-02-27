4041b246749b75626964439358a0cd480ce13ea4
OscarHMG <oscar_mg7@hotmail.com>
2016-02-27 14:25:18 -0500

    Adding Stack structure

--- EducationalProgram/EducationalProgram/main.c
+++ EducationalProgram/EducationalProgram/main.c
@@ -1,7 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define MAX 100
 typedef struct elementoLista {
     char *dato;
     struct ElementoLista *siguiente;
@@ -38,26 +38,38 @@ int  inputNumOfStudents();
 void printStadistics();
 void getFinalScore(char line[], int cont);
 void prinGraph(int score);
-
+void printWelcomeMessage();
+void headerInput();
+Pila* loadHeaders();
+Pila * inicializacion ();
+void imprimir_pila (Pila * tas);
 //Main Function
-
+struct Header *headers[MAX];
 int main(){
 	int num,i;
-	/*Header *info = createHeader();
-	inputHeaderInfo(info);
-	saveInfo(info);*/
+	Pila *pila= loadHeaders();
+	printWelcomeMessage();
+	headerInput();
 	num = inputNumOfStudents();
 	for(i =0; i<num;i++){
 		inputScores();
 	}
+	printf("\n\n");
 	system("cls");
 	printStadistics();
 	system("PAUSE");
 	return 0;
 }
 
-
-
+void headerInput(){
+	Header *info = createHeader();
+	inputHeaderInfo(info);
+	saveInfo(info);
+}
+void printWelcomeMessage(){
+	printf("\n\t\t\tSISTEMA DE CONTROL DE CALIFICACIONES\n");
+	printf("\t\t\tSeccion de Ingreso de Datos\n\n");
+}
 
 Header * createHeader(){
 	Header *tmp = (Header*)malloc(sizeof(Header));
@@ -148,7 +160,7 @@ void inputScores(){
 	scanf("%d",&ex_2P);
 	fflush(stdin);
 	saveScores(name,n1_1P,n2_1P,n3_1P,ex_1P,n1_2P,n2_2P,n3_2P,ex_2P);
-	system("cls");
+	printf("\t\t\t\nIngreso de estudiante ha sido exitoso...\n\n");
 }
 
 void saveScores(char *name,int n1_1P,int n2_1P,int n3_1P,int ex_1P,int n1_2P,int n2_2P,int n3_2P,int ex_2P){
@@ -168,8 +180,7 @@ void saveScores(char *name,int n1_1P,int n2_1P,int n3_1P,int ex_1P,int n1_2P,int
 
 int  inputNumOfStudents(){
 	int num;
-	system("cls");
-	printf("Cuantos estudiantes desea ingresar:");
+	printf("\n\nCuantos estudiantes desea ingresar:");
 	scanf("%d", &num);
 	fflush(stdin);
 	return num;
@@ -195,7 +206,7 @@ void printStadistics(){
 	}
 	fclose(file);
 }
-//Stack Functions
+
 
 void getFinalScore(char line[], int cont){
 	int i, cont_comma=0,j=0;
@@ -225,9 +236,70 @@ void prinGraph(int score){
 	}
 	printf(" %d \n",score);
 }
-void inicializacion (Pila * tas){
-  tas->inicio = NULL;
-  tas->tamanio = 0;
+
+Pila* loadHeaders(){
+	FILE *file;
+	int i = 0;
+	char line[400];
+	Header *h ;
+	int cont=0;
+	char career[50], subject[50], professor[50],n[50];
+	int course=0, semester=0;
+	char *p;
+	Pila *pila= inicializacion();
+	file = fopen("headers_info.txt","r");
+	if (file == NULL){
+		printf("\nError de apertura del archivo. \n\n");
+	}else{
+		while (feof(file) == 0){
+			h = createHeader();
+			fgets(line,400,file);
+			p = strtok(line,",");
+			while(p!=NULL){
+				strcpy(n,p);
+				p = strtok(NULL,",");
+				if(cont==0)
+					strcpy(h->career,n);
+				else if(cont==1)
+					h->semester=atoi(n);
+				else if(cont ==2)
+					h->course= atoi(n);
+				else if(cont ==3)
+					strcpy(h->professor,n);
+				else if(cont ==4)
+					strcpy(h->subject, n);
+				cont++;
+			}
+			cont =0;
+			headers[cont] = h; //Lleno una estructura de Headers
+			insertar_pila(pila,h->professor);
+	    }
+	}
+	fclose(file);
+	//imprimir_pila(pila);
+	return pila;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Stack Functions
+//------------------------------//
+Pila * inicializacion (){
+	Pila *tas = (Pila*)malloc(sizeof(Pila));
+	tas->inicio = NULL;
+	tas->tamanio = 0;
+	return tas;
 }
  
 /* apilar (a�adir) un elemento en la pila */
@@ -271,7 +343,153 @@ void imprimir_pila (Pila * tas){
   actual = tas->inicio;
  
   for(i=0;i<tas->tamanio;++i){
-    printf("\t%p - %s\n",actual, actual->dato);
+    printf("%s\n", actual->dato);
     actual = actual->siguiente;
   }
 }
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+typedef struct Elemento {
+    struct Header *contenido;
+    struct ElementoLista *siguiente;
+}Elemento;
+ 
+
+typedef struct Header{
+	char *career,*professor,*subject;
+	int course, semester;
+}Header;
+
+typedef struct Pila{
+  Elemento *inicio;
+  int tamanio;
+} Pila;
+ 
+Pila * inicializacion (){
+	Pila *tas = (Pila*)malloc(sizeof(Pila));
+	tas->inicio = NULL;
+	tas->tamanio = 0;
+	return tas;
+}
+ 
+int insertar_pila (Pila * tas, Header *h){
+  Elemento *nuevo_elemento;
+ 
+  if ((nuevo_elemento = (Elemento *) malloc (sizeof (Elemento))) == NULL)
+    return -1;
+  if ((nuevo_elemento->contenido = (Header *) malloc (sizeof (Header)))== NULL)
+    return -1;
+  nuevo_elemento->contenido = h;
+  //strcpy (nuevo_elemento->dato, dato);
+  nuevo_elemento->siguiente = tas->inicio;
+  tas->inicio = nuevo_elemento;
+  tas->tamanio++;
+  return 0;
+}
+ 
+ 
+int quitar_pila (Pila * tas){
+ 
+  Elemento *sup_elemento;
+ 
+  if (tas->tamanio == 0)
+    return -1;
+ 
+  sup_elemento = tas->inicio;
+  tas->inicio = tas->inicio->siguiente;
+ 
+  free (sup_elemento->contenido);
+  free (sup_elemento);
+  tas->tamanio--;
+  return 0;
+}
+ 
+void imprimir_pila (Pila * tas){
+  Elemento *actual;
+  int i;
+  actual = tas->inicio;
+  
+  for(i=0;i<tas->tamanio;++i){
+    //printf("\t%p - %s\n",actual, actual->dato);
+	  Header *h = actual->contenido;
+	  printf("Carrera: %s\nSemestre:%d\nCurso:%d\nProfesor:%s\nMateria:%s\n\n",h->career,h->semester,h->course,h->professor,h->subject);
+	  actual = actual->siguiente;
+  }
+}
+ 
+int searchHeader(Pila *pila, Header *h){
+	Elemento *actual;
+	int i;
+	actual = pila->inicio;
+	
+	for(i=0;i<pila->tamanio;++i){
+		Header *actual_h = actual->contenido;
+		if(strcmp(actual_h->career,h->career) && strcmp(actual_h->professor,h->professor) && strcmp(actual_h->subject,h->subject) && actual_h->course==h->course && actual_h->semester==h->semester){
+			return 1; //Existe en la pila
+		}
+		actual = actual->siguiente;
+  }
+	return 0; //No existe en la pila
+
+}
+Header * createHeader(){
+	Header *tmp = (Header*)malloc(sizeof(Header));
+	tmp->career = (char *)malloc(50 * sizeof(char));
+	tmp->professor = (char *)malloc(50 * sizeof(char));
+	tmp->subject = (char *)malloc(50 * sizeof(char));
+	tmp->course = -1;
+	tmp->semester = -1;
+	return tmp;
+}
+ 
+int main(int argc, char *argv[]){
+	Pila *pila = inicializacion();
+	Header *h1= createHeader();
+	Header *h2=  createHeader();
+	Header *h3= createHeader();
+	Header *h4= createHeader();
+	Header *h5= createHeader();
+	h1->career ="Ingieneria";
+	h1->course=5;
+	h1->professor="Caelos moreno";
+	h1->semester=8;
+	h1->subject="Programacion";
+	
+	h2->career ="Biologo";
+	h2->course=5;
+	h2->professor="Manuel Perez";
+	h2->semester=8;
+	h2->subject="Fisica";
+	
+	h3->career ="Economista";
+	h3->course=5;
+	h3->professor="Martha Lorenzo";
+	h3->semester=8;
+	h3->subject="Eocnoimia";
+	
+	h4->career ="Doctorado";
+	h4->course=5;
+	h4->professor="Marcos Tulio";
+	h4->semester=8;
+	h4->subject="Matematica";
+
+
+	h5->career ="Masterado";
+	h5->course=1;
+	h5->professor="Carlos Jordan";
+	h5->semester=8;
+	h5->subject="Matematica";
+	insertar_pila(pila,h1);
+	insertar_pila(pila,h2);
+	insertar_pila(pila,h3);
+	insertar_pila(pila, h4);
+	imprimir_pila(pila);
+	printf("Existe? : %d",searchHeader(pila,h5));
+	system("PAUSE");
+		return 0;
+}
+*/
